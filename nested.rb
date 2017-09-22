5a210d16bdbbaba612ceb1e233381cf0361a1bb3
Chris Nunez <cenunez47@gmail.com>
2017-09-22 22:31:47 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -128,6 +128,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-		 programmer_hash
+		 x =programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 x
 end
